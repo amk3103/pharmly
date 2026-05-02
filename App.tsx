9cc27b3fd0c598eceb72fb32bffbd73282a728da
amk3103 <id25m1017@iiitdm.ac.in>
2026-05-02 20:20:50 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import { RouterProvider } from "react-router";
-import { router } from "./routes";
+import React from "react";
+import Home from "./Home";
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return <Home />;
 }
